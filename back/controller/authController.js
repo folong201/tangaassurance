e2d8bf00876717f564c10a173a2cf172c4fd258b
folong201 <folong201@gmail.com>
2023-12-28 13:57:07 +0100

    update user

--- back/controller/authController.js
+++ back/controller/authController.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const User = require('../models/user');
+const User = require('../models/User.js');
 require('dotenv').config();
 
 exports.login = async (req, res, next) => {
